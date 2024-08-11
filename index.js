da3716534bf7298d9e6a4cc0c3d0282c285e6768
Jingwen Xia <amyfor7@gmail.com>
2024-08-11 17:32:27 +0800

    feat: allow CORS rules

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
